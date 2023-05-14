eb6901df4e9b9ec4baad3fce7bbc319d72b94eaa
Junaid Ahmed <junaidahmed9038@gmail.com>
2023-05-14 10:02:19 +0530

    remove node modules

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const expressEjsLayouts = require('express-ejs-layouts');
+require('./config/mongoose');
 
 
+
+
+app.use(express.urlencoded());// help to extract the data form body
+app.use(expressEjsLayouts);
 
 app.use('/',require('./routes/index'));
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 app.set('view engine','ejs');
 app.set('views','./views');
