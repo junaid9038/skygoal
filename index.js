0ce2e2f57b993f85ac9745c581f72b5576ddde9e
Junaid Ahmed <junaidahmed9038@gmail.com>
2023-05-14 09:44:20 +0530

    setup ejs

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 
 
+
+app.use('/',require('./routes/index'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(8000,function(err){
     if(err){
         console.log('error in the server',err);
